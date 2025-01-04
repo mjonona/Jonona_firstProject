9baa9411a3601eac7a07046fb560e9b410599d6d
mjonona <67279856+mjonona@users.noreply.github.com>
2025-01-04 07:25:26 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,4 @@ with st.sidebar:
   bill_lenght_mm = st.slider('Bill length (mm)', 13.1, 21.5, 17.3)
   flipper_length_mm = st.slider('Flipper length (mm)', 32.1, 59.6, 44.5)
   body_mass_g = st.slider('Body mass (g)', 32.1, 59.6, 44.5)
+  gender=st.selectbox('Gender', ('female', 'male'))
