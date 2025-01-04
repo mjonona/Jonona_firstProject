ee6cca95c400878af06de0ffe0504ea7e2fa05c4
mjonona <67279856+mjonona@users.noreply.github.com>
2025-01-04 07:10:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,9 @@ df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/pe
 
 with st.expander('Data'):
   st.write("X")
+  X_raw=df.drop('species', axis=1)
+  st.dataframe(X_raw)
+
+  st.write("y")
+  y_raw=df.species
+  st.dataframe(y_raw)
