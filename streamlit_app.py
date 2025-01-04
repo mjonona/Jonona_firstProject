2bcc8df12b9090b12f695287547ec5ce53857b88
mjonona <67279856+mjonona@users.noreply.github.com>
2025-01-04 07:05:51 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 My First Project')
 
 st.write('Final Project')
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+
+with st.expander('Data'):
+  st.write("X")
