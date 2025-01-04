ac3e835cd93cf0a173b59629fb513e8446d4a172
mjonona <67279856+mjonona@users.noreply.github.com>
2025-01-04 07:14:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title('🎈 My First Project')
 
-st.write('Final Project')
+st.write('Here, I will deploy a classification model.')
 df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
 
 with st.expander('Data'):
